4411e475058e2b9d3730ef794748c13c2d04471c
MaryamMehzabinZahin <zahinewu@gmail.com>
2022-03-18 14:18:51 +0600

    crud

---
+++ spring-project-with-database/src/main/java/com/maryam/demo/SpringProjectWithDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.maryam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectWithDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectWithDatabaseApplication.class, args);
+	}
+
+}
